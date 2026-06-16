27b66e91ad51fb551a89d1373471377b904fcc77
rodvillarg <jesus.villegas261186@potros.itson.edu.mx>
2026-06-15 19:23:00 -0700

    responsividad

---
+++ aurea/src/main/java/com/example/aurea/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+/**
+ * @author Jesús Rodrigo Villegas Argüelles - 261186
+ */
+package com.example.aurea.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
+
